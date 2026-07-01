2eac4ec19e664b8285ffd3aeedf71f6032c6b8b2
lcsjunior <lcsjuniorsi@gmail.com>
2026-07-01 11:28:02 -0300

    refactor: remove unused dBmToQuality helper

--- lib/commons/utilities.cpp
+++ lib/commons/utilities.cpp
@@ -2,14 +2,6 @@
 
 #define LOADING_DOT_INTERVAL_MS 250UL
 
-// clang-format off
-uint8_t dBmToQuality(const int16_t dBm) {
-  return (dBm <= -100) ? 0
-       : (dBm >= -50)  ? 100
-                       : 2 * (dBm + 100);
-}
-// clang-format on
-
 uint32_t getChipId() {
   uint32_t chipId = 0;
   for (int i = 0; i < 17; i = i + 8) {
--- lib/commons/utilities.h
+++ lib/commons/utilities.h
@@ -4,7 +4,6 @@
 #include <Arduino.h>
 #include <WiFi.h>
 
-uint8_t dBmToQuality(int16_t dBm);
 uint32_t getChipId();
 const char* getApName();
 const char* intToStr(int value);
